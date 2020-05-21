da384dbd88acf77fa77174cc7037aa47ca0f4f3e
Johselyne Miranda <johselynemiranda@gmail.com>
2020-05-21 01:48:25 -0400

    conexion

---
+++ Impuestos/Conf/DataBase.php
@@ -0,0 +1,29 @@
+<?php
+
+class Database
+{
+    // parametros para la conexion al SQL, nombre de host, nombre de bd
+    // nombre de usuarios y password
+    private $host = "localhost";
+    private $db_name = "Pandemials";
+    private $db_username = "root";
+    private $db_password = "1234";
+    public $connection;
+
+
+    //Funcion que realiza la conexion a la base
+    // la cual conecta con los parametros ya llamados
+    // con un try catch por si falla algo
+    // devuelve la conexion con la base
+    public function getConnection()
+    {
+        $this->connection = null;
+        try {
+            $this->connection = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name,
+                $this->db_username, $this->db_password);
+        } catch (PDOException $e) {
+            echo "Database connection error: " . $e->getMessage();
+        }
+        return $this->connection;
+    }
+}
--- Impuestos/EditarEliminar.php
+++ Impuestos/EditarEliminar.php
@@ -4,8 +4,18 @@
   <meta charset="utf-8">
   <title>Sign In</title>
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-  <link rel="stylesheet" href="C:\Users\User\Desktop\git\Pandemials\Impuestos\css\estilotabla.css">
+  <link rel="stylesheet" href="css\estilotabla.css">
   </head>
+
+  <?php
+   include "Conf\DataBase.php";
+    $db = new Database();
+    $dblink = $db->getConnection();
+    $sql= "SELECT Nombre FROM users WHERE nombre='Luis Fernando';";
+    $resultado= $dblink->query($sql);
+
+    $infoUs=$resultado->fetch();
+  ?>
 
   <body>
     <table>
@@ -17,7 +27,11 @@
       <tbody>
      <tr>
        <td>1</td>
-       <td>Montero Camila</td>
+       <td>
+         <?php echo $infoUs["Nombre"] ;
+
+
+          ?> </td>
        <td>
          <i class="material-icons button edit">edit</i>
          <i class="material-icons button delete">delete</i>
